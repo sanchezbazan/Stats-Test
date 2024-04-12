9ec4487fdca858f26658545d55447801d4afc522
brayan <brayansanchez464@gmail.com>
2024-04-12 11:28:02 -0700

    add core files

--- stats_core/core/utils/misc.py
+++ stats_core/core/utils/misc.py
@@ -1,11 +1,17 @@
 import yaml
+from django.conf import settings
+from django.db import transaction
+
+
+def apply_on_commit(callable_):
+    if settings.USE_ON_COMMIT_HOOK:
+        transaction.on_commit(callable_)
+    else:
+        callable_()
 
 
 def yaml_coerce(value):
-    # Convert value to proper Python
     if isinstance(value, str):
-        # yaml load returns a Python object (we are just creating some quick yaml data with a dummy)
-        # converts string to dict "{'apples': 1, 'bacon': 2,}" to python dict
-        # Useful because sometimes we need to stringify settings this way (Like in DockerFiles)
         return yaml.load(f'dummy: {value}', Loader=yaml.SafeLoader)['dummy']
+
     return value
