5773c87c2e520aeaa9ec800537cb3acf74514133
brayan <brayansanchez464@gmail.com>
2024-04-16 12:52:28 -0700

    FIXING ERRORS

--- stats_core/core/exceptions.py
+++ stats_core/core/exceptions.py
@@ -2,6 +2,7 @@ from rest_framework.exceptions import APIException
 
 
 def custom_exception_handler(exc, context):
+
     from rest_framework.views import exception_handler  # to avoid importing it too early
 
     if isinstance(exc, APIException):
